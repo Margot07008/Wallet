47c3985e609dfd311d062ea1ce3d3920b878d469
margot07008 <9311margarita@gmail.com>
2021-02-20 19:42:02 +0300

    fix style

--- src/App/pages/WelcomePage/SearchAddress/InputFormItem/InputFormItem.tsx
+++ src/App/pages/WelcomePage/SearchAddress/InputFormItem/InputFormItem.tsx
@@ -1,26 +1,28 @@
-import {Form, Input} from "antd";
-import * as React from "react";
+import { Form, Input } from 'antd';
+import * as React from 'react';
 
 const InputFormItem = () => {
     return (
-        <Form.Item initialValue="0x6f2e19b1dac147587ab8ad3a3ff556a5f2bb332a" name="address" label="Enter address here"
-                   rules={[{ required: true, message: 'Please input your address!'},
-                       () => ({
-                           validator(_, value) {
-                               const reg = new RegExp('0x[0-9a-fA-F]{40}');
-                               if (!value || value.search(reg) != -1) {
-                                   return Promise.resolve();
-                               }
-                               return Promise.reject('Enter a correct Eth address!');
-                           },
-                       }),
-                   ]}
+        <Form.Item
+            initialValue="0x6f2e19b1dac147587ab8ad3a3ff556a5f2bb332a"
+            name="address"
+            label="Enter address here"
+            rules={[
+                { required: true, message: 'Please input your address!' },
+                () => ({
+                    validator(_, value) {
+                        const reg = new RegExp('0x[0-9a-fA-F]{40}');
+                        if (!value || value.search(reg) != -1) {
+                            return Promise.resolve();
+                        }
+                        return Promise.reject('Enter a correct Eth address!');
+                    },
+                }),
+            ]}
         >
-            <Input allowClear
-                   placeholder="Example: 0x6f2e19b1dac147587ab8ad3a3ff556a5f2bb332a"
-            />
+            <Input allowClear placeholder="Example: 0x6f2e19b1dac147587ab8ad3a3ff556a5f2bb332a" />
         </Form.Item>
-    )
-}
+    );
+};
 
 export default InputFormItem;
--- src/App/pages/WelcomePage/SearchAddress/InputFormItem/index.ts
+++ src/App/pages/WelcomePage/SearchAddress/InputFormItem/index.ts
@@ -1,2 +1,1 @@
-
-export {default} from './InputFormItem'
+export { default } from './InputFormItem';
