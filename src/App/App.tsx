61dd545d1ab9756016620c4e94a91b81bfcf0590
margot07008 <9311margarita@gmail.com>
2021-02-25 01:54:44 +0300

    check vk

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -7,10 +7,16 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { urls } from '@config/apiUrls';
 import NotFoundPage from './pages/NotFoundPage';
 import { Spin } from 'antd';
-import { LoadingOutlined } from '@ant-design/icons';
 // @ts-ignore
 import LoaderIcon from 'react-loader-icon';
 import TransDetailsPage from './pages/TransDetailsPage';
+import bridge from '@vkontakte/vk-bridge';
+
+bridge.subscribe((e) => console.log(e));
+bridge.send("VKWebAppInit", {});
+if (bridge.supports("VKWebAppResizeWindow")) {
+    bridge.send("VKWebAppResizeWindow", {"width": 800, "height": 1000});
+}
 
 // Spin.setDefaultIndicator(<LoadingOutlined style={{color:'#d3adf7'}}/>)
 Spin.setDefaultIndicator(<LoaderIcon size={50} type={'spokes'} color="#d3adf7" />);
